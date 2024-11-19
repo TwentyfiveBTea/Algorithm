4e4f5a125e881a69ef954c2c5b94fb631a03aeb9
BTea <1248102041@qq.com>
2024-11-19 10:14:52 +0800

    2024.11.19 -- 41. 缺失的第一个正数

---
+++ LeetcodeQuestion/code241119.java
@@ -0,0 +1,29 @@
+package LeetcodeQuestion;
+
+import java.util.*;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2024/11/19 9:46
+ * @Description: 41. 缺失的第一个正数
+ */
+public class code241119 {
+    class Solution {
+        public int firstMissingPositive(int[] nums) {
+            Set<Integer> set = new HashSet();
+            int max = 0;
+
+            for (int num : nums) {
+                set.add(num);
+                max = Math.max(num, max);
+            }
+            for (int i = 1; i < max; i++) {
+                if (!set.contains(i)) {
+                    return i;
+                }
+            }
+
+            return max + 1;
+        }
+    }
+}
