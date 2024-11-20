b66acf4a464d4aaf8414e391912b738d29ac6fb7
BTea <1248102041@qq.com>
2024-11-20 10:41:29 +0800

    2024.11.20 -- 33. 搜索旋转排序数组

---
+++ LeetcodeQuestion/code241120_33.java
@@ -0,0 +1,49 @@
+package LeetcodeQuestion;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2024/11/20 10:40
+ * @Description: 33. 搜索旋转排序数组
+ * @Method: 二分查找
+ */
+public class code241120_33 {
+    class Solution {
+        public int search(int[] nums, int target) {
+            int index = 0;
+            int l = 0;
+            int r = nums.length - 1;
+            int ans = -1;
+            // 找出旋转点的下标
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    index = i + 1;
+                    break;
+                }
+            }
+
+            // 如果 target大于旋转点的数字，那就在右边找，反之
+            if (target >= nums[index] && target <= nums[nums.length - 1]) {
+                l = index;
+                r = nums.length - 1;
+            } else {
+                l = 0;
+                r = index - 1;
+            }
+
+            // 二分查找
+            while (l <= r) {
+                int mid = l + (r - l) / 2;
+                if (target > nums[mid]) {
+                    l = mid + 1;
+                } else if (target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    ans = mid;
+                    break;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
