dd6ad5e0c1780d595d3670ce12929d6d27d279b0
TwentyfiveBTea <1248102041@qq.com>
2024-12-06 13:15:41 +0800

    2024.12.06 -- 999. 可以被一步捕获的棋子数

---
+++ LeetcodeQuestion/Simulate/LeetCode_999.java
@@ -0,0 +1,49 @@
+package LeetcodeQuestion.Simulate;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2024/12/6 13:13
+ * @Description: 999. 可以被一步捕获的棋子数
+ */
+public class LeetCode_999 {
+    class Solution {
+        public int numRookCaptures(char[][] board) {
+            int x = 0;
+            int y = 0;
+            int ans = 0;
+            int[] dx = {-1, 0, 1, 0};
+            int[] dy = {0, 1, 0, -1};
+
+            for (int i = 0; i < 8; i++) {
+                for (int j = 0; j < 8; j++) {
+                    if (board[i][j] == 'R') {
+                        x = i;
+                        y = j;
+                        break;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int newX = x;
+                int newY = y;
+
+                while (true) {
+                    newX += dx[i];
+                    newY += dy[i];
+
+                    if (newX < 0 || newX >= 8 || newY < 0 || newY >= 8 || board[newX][newY] == 'B') {
+                        break;
+                    }
+                    if (board[newX][newY] == 'p') {
+                        ans++;
+                        break;
+                    }
+
+                }
+            }
+
+            return ans;
+        }
+    }
+}
