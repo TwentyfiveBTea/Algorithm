a1b6e14808634c0686a656abd14fe13641b638d9
TwentyfiveBTea <1248102041@qq.com>
2025-01-01 18:02:50 +0800

    2025.01.01 -- 3280. 将日期转换为二进制表示 (新年快乐!)

---
+++ LeetcodeQuestion/Math/LeetCode_3280.java
@@ -0,0 +1,21 @@
+package LeetcodeQuestion.Math;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/1/1 18:01
+ * @Description: 3280. 将日期转换为二进制表示
+ */
+public class LeetCode_3280 {
+    class Solution {
+        public String convertDateToBinary(String date) {
+            String[] dates = date.split("-");
+
+            for (int i = 0; i < dates.length; i++) {
+                dates[i] = Integer.toBinaryString(Integer.parseInt(dates[i]));
+            }
+
+            String res = String.join("-", dates);
+            return res;
+        }
+    }
+}
