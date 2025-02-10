8e133830680f157d7a32ce5f2b4fac33c8a9c690
BTea <1248102041@qq.com>
2025-02-10 13:46:49 +0800

    2025.02.10 -- P2241 - 统计方形（数据加强版）

---
+++ Luogu/Algorithm_1_3_Brute_forceEnumeration/P2089.java
@@ -0,0 +1,22 @@
+package Luogu.Algorithm_1_3_Brute_forceEnumeration;
+
+import java.util.Scanner;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2025/02/10 13:41
+ * @Description: 烤鸡
+ */
+public class P2089 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+        if(n < 10){
+            System.out.println(0);
+        }
+
+
+    }
+}
