7069825079af33ebbe246b0ae657ee9a96917b17
BTea <1248102041@qq.com>
2025-02-16 21:17:05 +0800

    2025.02.16 -- SQL -- 175. 组合两个表

--- LeetCode_SQL/LeetCode_SQL_175.sql
+++ LeetCode_SQL/LeetCode_SQL_175.sql
@@ -1,2 +1,3 @@
 # 175. 组合两个表
-select FirstName, LastName, City, State from Person left join Address on Person.personId = Address.personId;
+select FirstName, LastName, City, State
+from Person left join Address on Person.personId = Address.personId;
