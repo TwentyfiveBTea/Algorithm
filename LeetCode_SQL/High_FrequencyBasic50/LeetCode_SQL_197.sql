2ff39d4718ceb32e8dfe7e6b7d01f6f9804bb63a
BTea <1248102041@qq.com>
2025-02-20 01:18:53 +0800

    2025.02.20 -- SQL -- 197. 上升的温度

--- LeetCode_SQL/High_FrequencyBasic50/LeetCode_SQL_197.sql
+++ LeetCode_SQL/High_FrequencyBasic50/LeetCode_SQL_197.sql
@@ -1,4 +1,6 @@
 # 197. 上升的温度
 select w1.id
-from Weather as w1, Weather as w2
-where datediff(w1.recordDate, w2.recordDate) = 1 and w1.Temperature > w2.Temperature
+from Weather as w1,
+     Weather as w2
+where datediff(w1.recordDate, w2.recordDate) = 1
+  and w1.Temperature > w2.Temperature
