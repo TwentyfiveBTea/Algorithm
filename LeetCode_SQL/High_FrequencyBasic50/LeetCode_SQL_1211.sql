06d726011f144bef8ef6f2a07654f4704cfe590d
TwentyfiveBTea <1248102041@qq.com>
2025-02-27 11:36:23 +0800

    2025.02.27 -- SQL -- 1211. 查询结果的质量和占比

--- LeetCode_SQL/High_FrequencyBasic50/LeetCode_SQL_1211.sql
+++ LeetCode_SQL/High_FrequencyBasic50/LeetCode_SQL_1211.sql
@@ -1,4 +1,6 @@
 # 1211. 查询结果的质量和占比
-select query_name, round(avg(rating / position), 2) as quality, round(avg(rating < 3) * 100, 2) as poor_query_percentage
+select query_name,
+       round(avg(rating / position), 2) as quality,
+       round(avg(rating < 3) * 100, 2)  as poor_query_percentage
 from Queries
 group by query_name
